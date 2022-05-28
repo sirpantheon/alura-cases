d19dd29615490a9d900fa0a0c902bd3e8b467b1e
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-05-27 23:24:10 -0300

    primeiro pull alura cases

---
+++ pages/404.js
@@ -0,0 +1,17 @@
+import Link from '../src/components/link'
+
+
+export default function HomePage() {
+    return (
+      <div>
+        <h1>Alura Cases</h1>
+        <link href="/faq" >
+          voce se perdeu... e agora esta na pagina 404.
+        </link>
+      </div>
+      
+    )
+  }
+
+  
+  
---
+++ pages/faq.js
@@ -0,0 +1,14 @@
+import Link from '../src/components/link'
+
+export default function FaqPage() {
+    return (
+      <div>
+        <h1>pagina de perguntas</h1>
+        <Link href="/">
+          Home
+        </Link>
+      </div>
+      
+    )
+  }
+  
