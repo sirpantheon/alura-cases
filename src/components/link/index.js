8f53d79e0ef42368ca0b4f299d8fdf3dc446847c
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-06-02 00:32:10 -0300

    Update index.js

--- src/components/link/index.js
+++ src/components/link/index.js
@@ -3,7 +3,7 @@ import NextLink from 'next/link'
 export default function({children, href, ...props}){
     return (
         <NextLink href={href}>
-            <a {...props}></a>
+            <a {...props}>{children}</a>
         </NextLink>
     )
 }
