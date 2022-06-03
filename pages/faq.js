b38a298332645772ed8ea8f6c56602414faeaf16
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-06-03 16:03:40 -0300

    estrutura da pagina FAQ

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,43 @@
+import { useEffect, useState } from 'react';
 import Link from '../src/components/Link';
 
-export default function FAQPage() {
+export async function getStaticProps(){
+    const FAQ_API_URL = "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json"
+    const faq = await fetch(FAQ_API_URL)
+            .then((respostaDoServidor)=>{
+                return respostaDoServidor.json();
+            })
+            .then((resposta)=>{
+                return resposta
+            })
+    return{
+        props:{
+            qualquercoisa:"johnatan",
+            faq,
+        },
+    };
+}
+
+export default function FAQPage({faq}) {
+
     return (
         <div>
             <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
             <Link href="/">
                 Ir para a home
             </Link>
+            <ul>
+                {faq.map(({ answer, question }) => {
+                return (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                )
+                })}
+            </ul>
         </div>
     )
 }
