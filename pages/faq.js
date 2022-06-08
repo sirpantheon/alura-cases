69218a014a1b3fc723dfe8c545f7b6be241118bb
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-06-07 23:52:31 -0300

    final 3

--- pages/faq.js
+++ pages/faq.js
@@ -1,16 +1,7 @@
 import Head from 'next/head'
 import Link from '../src/components/Link';
 
-// SSG - Static Site Generation
-// SSR - Server Side Rendering
-// ISG - Incremental Static Generation
-
-// export async function getServerSideProps() {
-//     console.log('Em modo DEV, sempre roda! A cada acesso')
-//     console.log('Rodando a cada acesso que você recebe')
 export async function getStaticProps() {
-    console.log('Em modo DEV, sempre roda! A cada acesso')
-    console.log('Roda SOMENTE em build time')
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
         .then((respostaDoServidor) => {
@@ -22,14 +13,12 @@ export async function getStaticProps() {
 
     return {
       props: {
-          qualquercoisa: 'que eu passar aqui',
           faq,
       },
     };
 }  
 
 export default function FAQPage({ faq }) {
-    console.log('Isso roda no servidor???')
     return (
         <div>
             <Head>
